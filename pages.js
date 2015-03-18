220d45570ad277a7f624d3116df42bde22552c32
sedenardi <sedenardi@gmail.com>
2015-03-17 22:40:56 -0400

    fix pages table

--- pages.js
+++ pages.js
@@ -2,7 +2,7 @@ var SqlIngester = require('./sqlIngester.js');
 
 var ingester = new SqlIngester({
   fileName: '/run/media/sanders/1TB/wikidump/enwiki-20150304-page.sql',
-  queryString: 'INSERT INTO `page2` (`page_id`,`page_title`,`page_is_redirect`,`page_len`) VALUES ?',
+  queryString: 'INSERT INTO `page` (`page_id`,`page_title`,`page_is_redirect`,`page_len`) VALUES ?',
   insertFunction: function(chunk) {
     return [chunk[0],chunk[2],chunk[5],chunk[10]];
   },
